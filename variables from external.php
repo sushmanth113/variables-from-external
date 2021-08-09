20473811020dec4bf3537a09b9202b532f4f949e
sushmanth113 <86585994+sushmanth113@users.noreply.github.com>
2021-08-09 18:24:45 +0530

    Add files via upload

---
+++ variables from external.php
@@ -0,0 +1,9 @@
+<form action="variables from external.php" method="post">
+    Name:  <input type="text" name="username" /><br />
+    Email: <input type="text" name="email" /><br />
+    <input type="submit" name="submit" value="Submit me!" />
+</form>
+<?php
+echo $_POST['username'];
+echo $_REQUEST['username'];
+?>
